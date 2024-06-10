62f1b04c497a4da79dde71e697b248e751cb9cb4
fahriel <fahrielfaldi@gmail.com>
2024-06-10 16:09:00 +0700

    first commit

---
+++ src/test/java/com/astratech/_C_FahrielDwifaldi_0320220086/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astratech._C_FahrielDwifaldi_0320220086;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
